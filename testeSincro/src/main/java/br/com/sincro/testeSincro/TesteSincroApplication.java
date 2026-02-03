bedd67400778d6928483cc1173fbc2391a756020
RodrigoBatis <rodrigofreirebatista@gmail.com>
2026-02-02 21:32:18 -0300

    feat:Adicionando a estrutura base do projeto.

---
+++ testeSincro/src/main/java/br/com/sincro/testeSincro/TesteSincroApplication.java
@@ -0,0 +1,13 @@
+package br.com.sincro.testeSincro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSincroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSincroApplication.class, args);
+	}
+
+}
