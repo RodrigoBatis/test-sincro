daf84716e13bc506d92949da5e6de43a87f97405
RodrigoBatis <rodrigofreirebatista@gmail.com>
2026-02-03 22:03:51 -0300

    Inserindo validação para não limpar dados que não forem escritos no momneto da alteração

--- testeSincro/src/main/java/br/com/sincro/testeSincro/controller/ItemController.java
+++ testeSincro/src/main/java/br/com/sincro/testeSincro/controller/ItemController.java
@@ -37,25 +37,61 @@ public class ItemController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Item> update(@PathVariable Long id,
-                                       @Validated @RequestBody Item item) {
+                                       @RequestBody Item item) {
 
         Item existing = service.findById(id);
 
-        existing.setName(item.getName());
-        existing.setSku(item.getSku());
-        existing.setDescription(item.getDescription());
-        existing.setCategory(item.getCategory());
-        existing.setLocation(item.getLocation());
-        existing.setDate(item.getDate());
-        existing.setDepreciationMethod(item.getDepreciationMethod());
-        existing.setDepreciationRate(item.getDepreciationRate());
-        existing.setResidualValue(item.getResidualValue());
-        existing.setQuantity(item.getQuantity());
-        existing.setMinimiumStock(item.getMinimiumStock());
-        existing.setUnitPrice(item.getUnitPrice());
+        if (item.getName() != null && !item.getName().isBlank()) {
+            existing.setName(item.getName());
+        }
+
+        if (item.getSku() != null) {
+            existing.setSku(item.getSku());
+        }
+
+        if (item.getDescription() != null) {
+            existing.setDescription(item.getDescription());
+        }
+
+        if (item.getCategory() != null) {
+            existing.setCategory(item.getCategory());
+        }
+
+        if (item.getLocation() != null) {
+            existing.setLocation(item.getLocation());
+        }
+
+        if (item.getDate() != null) {
+            existing.setDate(item.getDate());
+        }
+
+        if (item.getDepreciationMethod() != null) {
+            existing.setDepreciationMethod(item.getDepreciationMethod());
+        }
+
+        if (item.getDepreciationRate() != null) {
+            existing.setDepreciationRate(item.getDepreciationRate());
+        }
+
+        if (item.getResidualValue() != null) {
+            existing.setResidualValue(item.getResidualValue());
+        }
+
+        if (item.getQuantity() > 0) {
+            existing.setQuantity(item.getQuantity());
+        }
+
+        if (item.getMinimiumStock() > 0) {
+            existing.setMinimiumStock(item.getMinimiumStock());
+        }
+
+        if (item.getUnitPrice() != null) {
+            existing.setUnitPrice(item.getUnitPrice());
+        }
 
         return ResponseEntity.ok(service.save(existing));
     }
+
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void>delete(@PathVariable Long id){
